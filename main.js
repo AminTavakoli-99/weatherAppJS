a9c5f11105f087d39f93d745d1d6ebc183eb7c46
AminTavakoli-99 <amtaso.232@gmail.com>
2025-11-09 10:55:35 +0330

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+    //paste your apiKey from https://openweathermap.org/ 
+    const apiKey ="";
+    const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric";
+
+    const searchBox = document.querySelector('.search input');
+    const searchBtn = document.querySelector('.search button');
+    const weatherIcon = document.querySelector(".weather-icon");
+
+    async function checkWeather(city) {
+        const response = await fetch(apiURL+`&q=${city}&appid=${apiKey}`);
+        if (response.status == 404) {
+            document.querySelector('.error').style.display = 'block';
+            document.querySelector('.weather').style.display = 'none';
+        }else{
+            var data = await response.json();
+            
+            document.querySelector(".city").innerHTML = data.name;
+            document.querySelector(".temp").innerHTML = `${Math.round(data.main.temp)} °C`;
+            document.querySelector(".humidity").innerHTML = `${data.main.humidity} %`;
+            document.querySelector(".wind").innerHTML = `${data.wind.speed} m/s`;
+            if (data.weather[0].main == 'Clear') {
+                weatherIcon.src = 'images/clear.png';
+            }else if (data.weather[0].main == 'Clouds') {
+                weatherIcon.src = 'images/clouds.png';  
+            }else if(data.weather[0].main == 'Drizzle'){
+                weatherIcon.src = 'images/drizzle.png';
+            }else if(data.weather[0].main == 'Mist'){
+                weatherIcon.src = 'images/mist.png';
+            }else if(data.weather[0].main == 'Rain'){
+                weatherIcon.src = 'images/rain.png';
+            }else if(data.weather[0].main == 'Snow'){
+                weatherIcon.src = 'images/snow.png';
+            }
+            document.querySelector('.error').style.display = "none";
+            document.querySelector('.weather').style.display = "block";
+        }
+        
+        
+    }
+    
+    searchBtn.addEventListener('click',(e)=>{
+        checkWeather(searchBox.value);
+    })
